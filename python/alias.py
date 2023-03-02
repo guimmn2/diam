fa906ea3503f0c08ba90273f859a3da3c0fea851
guimmn2 <gmnss@protonmail.com>
2023-03-02 14:52:11 +0000

    falta resposta do prof e ultimo ex

--- python/alias.py
+++ python/alias.py
@@ -1,18 +1,16 @@
 import time
+from itertools import permutations
 
-str1 = "the alias men"
-str2 = "alan smithee"
+str1 = "r   oma"
+str2 = "amo r"
 
 
 #O(n²)
 def none_approach(word1, word2):
     aux = [*word2.replace(" ", "")]
-    print(len(aux))
-    print(aux)
     counter = 0
     for char in word1:
         if char != " " and char in word2:
-            print(char)
             for i in range(len(aux)):
                 if aux[i] == char:
                     aux[i] = "None"
@@ -25,6 +23,7 @@ def none_approach(word1, word2):
 print(str(none_approach(str1, str2)))
 
 #O(2n*logn)
+#Provavelmente teremos que refazer este... À espera da resposta do prof
 def sort_approach(word1, word2):
     aux1 = sorted([*word1.replace(" ", "")])
     aux2 = sorted([*word2.replace(" ", "")])
@@ -33,5 +32,36 @@ def sort_approach(word1, word2):
 print(sort_approach(str1, str2))
 
 
+#Provavelmente teremos que refazer este... À espera da resposta do prof
+def brute_force_approach(word1, word2):
+    word1 = word1.replace(" ", "")
+    word2 = word2.replace(" ", "")
+    return word2 in [''.join(p) for p in permutations(word1)]
 
+print(brute_force_approach(str1, str2))
 
+#3 passos (no pior cenário)
+def word_count_approach(word1, word2):
+    word1 = word1.replace(" ", "")
+    word2 = word2.replace(" ", "")
+    w1 = {}
+    w2 = {}
+    for c in word1:
+        if w1.get(c) != None:
+            w1[c] += 1
+        else:
+            w1[c] = 1
+    for c in word2:
+        if w2.get(c) != None:
+            w2[c] += 1
+        else:
+            w2[c] = 1
+
+    for k in w1.keys():
+        if w2.get(k) == None:
+            return False
+        if w2.get(k) != w1.get(k):
+            return False
+    return True
+
+print(word_count_approach(str1, str2))
