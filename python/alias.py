d4af3603f65954d726ce33249f9ae7dd7c297f8d
guimmn2 <gmnss@protonmail.com>
2023-03-02 13:02:09 +0000

    none_approach

--- python/alias.py
+++ python/alias.py
@@ -1,4 +1,26 @@
 import time
 
-str1 = "roma"
-str2 = "amor"
+str1 = "thealiasmen"
+str2 = "alan smithee"
+
+
+def none_approach(word1, word2):
+    aux = [*word2.replace(" ", "")]
+    print(len(aux))
+    print(aux)
+    counter = 0
+    for char in word1:
+        if char != " " and char in word2:
+            print(char)
+            for i in range(len(aux)):
+                if aux[i] == char:
+                    aux[i] = "None"
+                    counter += 1
+    return counter == len(aux)
+
+    
+
+
+print(none_approach(str1, str2))
+
+
